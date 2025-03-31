0c8bf7eeac176ce3134a5202d91aaf9b23bfa484
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2025-03-31 23:05:13 +0200

    GUI追加

--- src/initGUI.js
+++ src/initGUI.js
@@ -3,7 +3,7 @@
  * Sets up the user interface controls for manipulating video and image processing parameters
  */
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js'
-import { anaglyphMacros, filterMacros } from './shader.js'
+import { anaglyphMacrosName, filterMacrosName } from './shader.js'
 import { ImageProcessingMaterialController } from './imageProcessingController.js'
 import { VideoController } from './videoElement.js'
 
@@ -63,14 +63,14 @@ export function initGUI(gui, materialCtrl, videoCtrl) {
   // Filter parameters controls
   const filterParamFolder = gui.addFolder('Filter Parameters')
   filterParamFolder
-    .add(materialCtrl.uniforms.kernelSizeDiv2, 'value', 1, 10, 1)
+    .add(materialCtrl.uniforms.kernelSizeDiv2, 'value', 1, 20, 1)
     .name('Kernel Size / 2')
   filterParamFolder.add(materialCtrl.uniforms.sigma, 'value', 0.1, 10).name('Sigma')
 
   // Anaglyph method selection folder
   const anaglyphGUI = gui.addFolder('Anaglyph')
   anaglyphGUI
-    .add({ value: Object.keys(anaglyphMacros)[0] }, 'value', Object.keys(anaglyphMacros))
+    .add({ value: anaglyphMacrosName[0] }, 'value', anaglyphMacrosName)
     .name('Method')
     .onChange((name) => {
       materialCtrl.onAnaglyphChange(name)
@@ -81,25 +81,10 @@ export function initGUI(gui, materialCtrl, videoCtrl) {
   const nbFilter = materialCtrl.nbFilter
   for (let i = 0; i < nbFilter; i++) {
     filtersFolder
-      .add({ value: Object.keys(filterMacros)[0] }, 'value', Object.keys(filterMacros))
+      .add({ value: filterMacrosName[0] }, 'value', filterMacrosName)
       .name(`Filter ${i + 1}`)
       .onChange((name) => {
         materialCtrl.onFilterChange(name, i)
       })
   }
-
-  // filtersFolder
-  //   .add({ value: Object.keys(filterMacros)[0] }, 'value', Object.keys(filterMacros))
-  //   .name('Filter 1')
-  //   .onChange((name) => {
-  //     materialCtrl.onFilterChange(name, 0)
-  //   })
-
-  // // Second filter selection folder
-  // filtersFolder
-  //   .add({ value: Object.keys(filterMacros)[0] }, 'value', Object.keys(filterMacros))
-  //   .name('Filter 2')
-  //   .onChange((name) => {
-  //     materialCtrl.onFilterChange(name, 1)
-  //   })
 }
